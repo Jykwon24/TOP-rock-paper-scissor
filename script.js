43b089791eb14f6b060c6e2caf96432dcd3aa863
Jeff Kwon <jykwon007@gmail.com>
2024-04-17 14:55:00 -0700

    Add updatescore and updatemessage functions to eventhandler

--- script.js
+++ script.js
@@ -22,9 +22,82 @@ const playerScore2 = document.querySelector('.player-score')
 const computerScore2 = document.querySelector('.computer-score')
 
 
-btnContainer.addEventListener('click', function (e) {
+btnContainer.addEventListener('click', handleClick)
+
+function getComputerChoice() {
+  const resultArr = ['rock', 'paper', 'scissors']
+  return resultArr[getRandom(3)]
+}
+
+function getRandom(max) {
+  return Math.floor(Math.random() * max)
+}
+
+function playGame(computerSelection, playerSelection) {
+  console.log('Computer choice:', computerSelection)
+  console.log('Player choice:', playerSelection)
+
+  if (playerSelection === computerSelection) {
+    roundWinner = 'draw'
+    updateMessage(playerSelection, computerSelection, roundWinner)
+    return updateScore()
+  }
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    console.log('Player Wins')
+    roundWinner = 'player'
+    playerScore++
+    updateMessage(playerSelection, computerSelection, roundWinner)
+    return updateScore()
+  }
+  console.log('Computer Wins!')
+  computerScore++
+  roundWinner = 'computer'
+  updateMessage(playerSelection, computerSelection, roundWinner)
+  updateScore()
+}
+
+
+
+function updateMessage(playerChoice, computerChoice, roundWinner) {
+  switch (roundWinner) {
+    case 'player':
+      roundResult.textContent = `${capitalizeLetter(playerChoice)} beats ${capitalizeLetter(computerChoice)}, you win!`
+      break;
+    case 'computer':
+      roundResult.textContent = `${capitalizeLetter(computerChoice)} beats ${capitalizeLetter(playerChoice)}, you lost this one!`
+      break;
+    case 'draw':
+      roundResult.textContent = "It's a draw, play again!"
+      break;
+  }
+  console.log('player choice in update message:', playerChoice)
+  console.log('computer choice in update message:', computerChoice)
+  console.log('round winner in update message:', roundWinner)
+}
+
+function updateScore() {
+  switch (roundWinner) {
+    case 'player':
+      playerScore2.textContent = `${playerScore}`
+      break;
+    case 'computer':
+      computerScore2.textContent = `${computerScore}`
+      break;
+  }
+}
+
+function capitalizeLetter(input) {
+  return input.charAt(0).toUpperCase() + input.toLowerCase().slice(1)
+}
+
+function handleClick(e) {
   let targetEvent = e.target.getAttribute('class')
   let currentTargetEvent = e.currentTarget.getAttribute('class')
+  // console.log(targetEvent)
 
   switch (targetEvent) {
     case 'rock':
@@ -40,97 +113,9 @@ btnContainer.addEventListener('click', function (e) {
       playGame(getComputerChoice(), targetEvent)
       break;
   }
-})
 
-function getComputerChoice() {
-  const resultArr = ['rock', 'paper', 'scissors']
-  return resultArr[getRandom(3)]
-}
-
-function getRandom(max) {
-  return Math.floor(Math.random() * max)
-}
-
-// function playerChoice(input) {
-//   if (typeof input !== 'string') {
-//     return 'Please enter a valid string'
-//   }
-
-//   if (input.toLowerCase() === 'rock' || input.toLowerCase() === 'paper' || input.toLowerCase() === 'scissors') {
-//     return input.charAt(0).toUpperCase() + input.toLowerCase().slice(1)
-
-//   }
-//   return 'Input must either be rock, paper or scissors'
-// }
-
-function playGame(computerSelection, playerSelection) {
-  console.log('Computer choice:', computerSelection)
-  console.log('Player choice:', playerSelection)
-
-  if (playerSelection === computerSelection) return roundWinner = 'draw'
-
-  if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-  ) {
-    console.log('Player Wins')
-    roundWinner = 'player'
-    playerScore++
-    console.log('Round Winner:', roundWinner)
-  }
-  console.log('Computer Wins!')
-  computerScore++
-  roundWinner = 'computer'
-}
-
-function updateMessage(playerChoice, computerChoice, roundWinner) {
-  switch (roundWinner) {
-    case 'player':
-      roundResult.textContent = `${capitalizeLetter(playerChoice)} beats ${capitalizeLetter(computerChoice)}, you win!`
-      break;
-    case 'computer':
-      roundResult.textContent = `${capitalizeLetter(computerChoice)} beats ${capitalizeLetter(playerChoice)}, you lost this one!`
-      break;
-    case 'draw':
-      roundResult.textContent = "It's a draw, play again!"
-      break;
-  }
-}
-
-function updateScore(roundWinner) {
-  switch (roundWinner) {
-    case 'player':
-
-
-  }
-}
-
-function capitalizeLetter(input) {
-  return input.charAt(0).toUpperCase() + input.toLowerCase().slice(1)
-}
-function displayResult() {
-  const stringUpdate = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1)
-
-  if (stringUpdate === computerSelection) {
-    return roundResult.textContent = "It's a tie! Play Again!"
-  }
-
-  if ((stringUpdate === 'Rock' && computerSelection === 'Scissors') || (stringUpdate === 'Paper' && computerSelection === 'Rock') || (stringUpdate === 'Scissors' && computerSelection === 'Paper')) {
-    console.log('Player Wins')
-    console.log('playerScore in function:', playerScore)
-    return roundResult.textContent = "You Win!"
-  }
-  computerScore++
-  return console.log('Computer Wins!')
+  console.log('Player Score:', playerScore)
+  console.log('Computer Score:', computerScore)
+  console.log('Round Winner:', roundWinner)
 
 }
-
-function startRound() {
-  playGame(getComputerChoice(), playerChoice())
-
-  console.log('Player Score: ', playerScore)
-  console.log('Computer Score: ', computerScore)
-}
-
-// playRound()
